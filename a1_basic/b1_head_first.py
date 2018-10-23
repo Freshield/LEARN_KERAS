f42bc54837b15698129e705a51e5415c995a014a
Freshield <zxdsw199182@gmail.com>
2018-10-23 15:25:31 +0800

    basic idea

--- a1_basic/b1_head_first.py
+++ a1_basic/b1_head_first.py
@@ -15,3 +15,31 @@
 @                                    Freshield @
 @==============================================@
 """
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.optimizers import SGD
+
+x_train = np.zeros((512,512))
+y_train = np.zeros((10,))
+x_batch = x_train
+y_batch = y_train
+x_test = x_train
+y_test = y_train
+
+model = Sequential()
+
+model.add(Dense(units=64, input_dim=100))
+model.add(Activation('relu'))
+model.add(Dense(units=10))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.01, momentum=0.9, nesterov=True), metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5, batch_size=32)
+
+# model.train_on_batch(x_batch, y_batch)
+
+loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
+
+classes = model.predict(x_test, batch_size=128)
---
+++ a1_basic/b2_basic_idea.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@License: (C) Copyright 2018, BEIJING LINKING MEDICAL TECHNOLOGY CO., LTD.
+@Contact: yangyufresh@163.com
+@File: b2_basic_idea.py
+@Time: 18-10-23 15:24
+@Last_update: 18-10-23 15:24
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
