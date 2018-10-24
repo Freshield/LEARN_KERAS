03b30d58205dc0ee2bf34a358fb893695febf1bb
Freshield <zxdsw199182@gmail.com>
2018-10-24 14:50:41 +0800

    head first

---
+++ a1_basic/b1_head_first.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@License: (C) Copyright 2018, BEIJING LINKING MEDICAL TECHNOLOGY CO., LTD.
+@Contact: yangyufresh@163.com
+@File: b1_head_first.py
+@Time: 18-10-24 14:50
+@Last_update: 18-10-24 14:50
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
